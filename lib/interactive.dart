11a03a092182a30a9d6089f211fa226cbe0e48e5
Otto Järvenpää <otto.jarvenpaa@edu.tampere.fi>
2025-03-30 17:50:27 +0300

    game nearly finished

--- lib/interactive.dart
+++ lib/interactive.dart
@@ -1,32 +1,278 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MyInteractiveText extends StatefulWidget {
+
+final kysymykset = [
+  "Kun lapsen jalat koskettavat alastulossa trampoliinin pintaa, trampoliini alkaa hidastaa lapsen vauhtia. Mikä seuraavista väittämistä pätee tilanteessa?", 
+  "Placeholder",
+  "Placeholder",
+  "Väite ”Yksikään aviopuoliso ei ole naimaton” ilmaisee Immanuel Kantin mukaan",
+  "Funktion derivaatan arvo kertoo …",
+  "Mitkä arvot lukujono tuottaa?",
+  "Täytä tyhjä: Interaction competence is developed through the _____ working methods and techniques of physical education.",
+  "Kahden vektorin pistetulo on aina",
+  "Placeholder",
+  "Katuporan äänen intensiteettitaso 10 metrin etäisyydellä porasta on 100 dB. Kuinka suuri on kahden katuporan yhdessä tuottaman äänen intensiteettitaso samalla etäisyydellä?",
+  "Mikä lause kuuluu tyhjään kohtaan?",
+  "Mikä sana tulee tyhjään kohtaan: 2016 konnte ich einen Monat als Gastschüler in Finnland verbringen. Mir hat es hier gleich sehr gut",
+  "Kahdesta käämistä koostuvalla muuntajalla muutetaan",
+  "Täytä tyhjä: _______ tarkoittaa jonkin teorian tai väitteen kumoamista tai osoittamista vääräksi.",
+  "What is the environment like in the novel:",
+  "Valitse vastaavin vaihtoehto alleviivatulle sanalle: For centuries Western culture has been permeated by the idea that humans are selfish creatures. ",
+  "Millainen päätelmä on: Jos Musti on kala, se osaa hengittää veden alla. Musti ei osaa hengittää veden alla, siispä se ei ole kala.",
+  "Mikä sana tulee tyhjään kohtaan: Lennart, 17, aus Berlin erzählt über sein Hobby: „Beatboxer kennen ____ unter dem Namen Lennsi. [...]”",
+  ];
+final vastauksetA = [
+  "Lapsi kohdistaa trampoliiniin suuremman voiman kuin trampoliini lapseen.", 
+  "Placeholder",
+  "Placeholder",
+  "analyyttistä apriorista tietoa.",
+  "funktion suurimman arvon",
+  "(1, 2, 3, 4, …)",
+  "diversifying",
+  "Kokonaisluku",
+  "Placeholder",
+  "110 dB",
+  "ob sie vielleicht Lust hat",
+  "gefielt",
+  "tasajännitteen suuruutta",
+  "Deduktio",
+  "Chaotic",
+  "complicated",
+  "Analogiapäätelmä",
+  "mir",
+  ];
+final vastauksetB = [
+  "Lapsi ja trampoliini eivät kohdista voimaa toisiinsa.", 
+  "Placeholder",
+  "Placeholder",
+  "synteettistä apriorista tietoa",
+  "funktion maksimikohdan",
+  "(1, 3, 5, 7, …)",
+  "diverted",
+  "Reaaliluku",
+  "Placeholder",
+  "103 dB",
+  "passt sie gut zusammen",
+  "gefallen",
+  "vaihtojännitteen suuruutta",
+  "Demarkaatio",
+  "Vibrant",
+  "invaded",
+  "Ehdollinen(hypoteettinen) syllogismi",
+  "mein",
+  ];
+final vastauksetC = [
+  "Lapsi kohdistaa trampoliiniin pienemmän voiman kuin trampoliini lapseen.", 
+  "Placeholder",
+  "Placeholder",
+  "synteettistä aposteriorista tietoa.",
+  "funktion kuvaajan tangentin kulmakertoimen",
+  "(2, 4, 6, 8, …)",
+  "diverse",
+  "Vektori",
+  "Placeholder",
+  "200 dB",
+  "dass sie mich auch gut findet",
+  "gefällt",
+  "vaihtojännitteen taajuutta",
+  "Falsifointi",
+  "Tranquil",
+  "tarnished",
+  "Käytännöllinen(praktinen) syllogismi",
+  "mich",
+  ];
+final vastauksetD = [
+  "Lapsi ja trampoliini kohdistavat toisiinsa saman suuruiset voimat.", 
+  "Placeholder",
+  "Placeholder",
+  "Placeholder",
+  "funktion kuvaajan ja x-akselin välisen alueen pinta-alan",
+  "(2, 4, 8, 16, …)",
+  "pp",
+  "suora",
+  "Placeholder",
+  "141 dB",
+  "was passiert auch leicht",
+  "placheholder",
+  "placheholder",
+  "Paradigma",
+  "Paradigma",
+  "Takajäsenen kielto, modus tollens",
+  "Takajäsenen kielto, modus tollens",
+  ];
+
+final imgNames = [
+  "q13.png", 
+  "YOlakkisuora.webp",
+  "YOlakkisuora.webp",
+  "YOlakkisuora.webp",
+  "q8.png",
+  "q8.png",
+  //a
+  "q8.png",
+  "q8.png",
+  "q8.png",
+  "q8.png",
+  "q11.png",
+  "q11.png",
+  "q11.png",
+  "q11.png",
+  "q3.png",
+  "q3.png",
+  "q3.png",
+  "q3.png",
+  ];
+final imgSizesh = [
+  505.0,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  30.0,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  123.0,
+  0.0,
+  0.0,
+  0.0,
+  250.0,
+  0.0,
+  0.0,
+  0.0,
+ ];
+final imgSizesw = [
+  271.0, 
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  318.0,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+  647.0,
+  0.0,
+  0.0,
+  0.0,
+  558.0,
+  0.0,
+  0.0,
+  0.0,
+];
+
+final options = [4, 4, 4, 3, 4, 4, 3, 4, 4, 4, 4, 3, 3, 4, 3, 3, 4, 3];
+
+final vastauksetOikea = [4, 1, 1, 1, 3, 3, 3, 2, 1, 2, 3, 2, 2, 3, 3, 2, 4, 3];
+
+final introTeksti = Text('Tässä tälläinen pikkupeli. En jage kirjoittaa tätä vielä, niin kirjoittelen sen varmaan myöhemmin!!', 
+  style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255)),textAlign: TextAlign.center,);
+
+int counter = 0;
+
+class SYOpeli extends StatefulWidget {
   @override
-  _MyInteractiveTextState createState() => _MyInteractiveTextState();
+  _SYOpeliState createState() => _SYOpeliState();
 }
 
-class _MyInteractiveTextState extends State<MyInteractiveText> {
-  Color textColor = const Color.fromARGB(255, 255, 0, 0);
+class _SYOpeliState extends State<SYOpeli> {
+  int counter = 0;
+  int nextB = 0;
+  int oikeat = 0;
+  int vaarat = 0;
+  List<int> vastauksetLog = [];
+
+  Column testChange = Column(children: [introTeksti]);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          'Interactive text',
-          style: TextStyle(color: textColor),
+        if(nextB == 0) testChange,
+        if(nextB == 0) IconButton(
+            icon: const Icon(Icons.arrow_forward),
+            color: const Color.fromARGB(255, 255, 255, 255),
+            tooltip: 'Aloita',
+            onPressed: () {
+              setState(() {
+                nextB = 1;
+              });
+            },
         ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              textColor =
-                  textColor == Colors.black ? Colors.red : Colors.black;
-            });
-          },
-          child: Text('Change Color'),
-        ),
+        if(nextB == 1 && counter < 18) Column(
+            children: [
+              Text(kysymykset[counter], 
+              style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255)), softWrap: true, textAlign: TextAlign.center,
+              ),
+              Image(image: AssetImage('assets/images/' + imgNames[counter]), height: imgSizesh[counter], width: imgSizesw[counter],),
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  if (vastauksetOikea[counter] == 1){oikeat++;}
+                  else{vaarat++;}
+                  counter++;
+                  vastauksetLog.add(1);
+                  });},
+                child: Text(vastauksetA[counter])),
+              SizedBox(height: 10),
+              if(options[counter] > 1) ElevatedButton(onPressed: (){
+                setState(() {
+                  if (vastauksetOikea[counter] == 2){oikeat++;}
+                  else{vaarat++;}
+                  counter++;
+                  vastauksetLog.add(2);
+                  });}, 
+                  child: Text(vastauksetB[counter])),
+              if(options[counter] > 1) SizedBox(height: 10),
+              if(options[counter] > 2) ElevatedButton(onPressed: (){
+                setState(() {
+                  if (vastauksetOikea[counter] == 3){oikeat++;}
+                  else{vaarat++;}
+                  counter++;
+                  vastauksetLog.add(3);
+                  });}, 
+                  child: Text(vastauksetC[counter])),
+              if(options[counter] > 2)SizedBox(height: 10),
+              if(options[counter] > 3)ElevatedButton(onPressed:  (){
+                setState(() {
+                  if (vastauksetOikea[counter] == 4){oikeat++;}
+                  else{vaarat++;}
+                  counter++;
+                  vastauksetLog.add(4);
+                  });}, 
+                  child: Text(vastauksetD[counter])),
+              SizedBox(height: 10),
+              Row(children: [
+                Text(oikeat.toString(), style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 0, 255, 42), fontWeight: FontWeight.bold)),
+                SizedBox(width: 5),
+                Text((counter+1).toString()+'/20', style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255)),),
+                SizedBox(width: 5),
+                Text(vaarat.toString(), style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 0, 0), fontWeight: FontWeight.bold)),
+                ], mainAxisAlignment: MainAxisAlignment.center,),
+              SizedBox(height: 10),
+            ],
+
+          ),
+        if(nextB == 1 && counter >= 18 && oikeat > 17)
+        Column(children: [
+          Text('Ehdottomasti!', style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255))),
+          Text('Sait ' + oikeat.toString() + ' oikein. Se on ' + (oikeat-17).toString() + ' enemmän kuin ylioppilas Otto. Erinomainen suoritus!', style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255)),  textAlign: TextAlign.center),
+        ],),
+        if(nextB == 1 && counter >= 18 && oikeat == 17)
+        Column(children: [
+          Text('Ehkäpä!', style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255))),
+          Text('Sait 17 oikein. Se on yhtä paljon, kuin ylioppilas Otolla. Hyvää työtä!', style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255)), textAlign: TextAlign.center),
+        ],),
+        if(nextB == 1 && counter >= 18 && oikeat < 17)
+        Column(children: [
+          Text('Ei aivan!', style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255))),
+          Text('Sait ' + oikeat.toString() + ' oikein. Se on ' + (17-oikeat).toString() + ' vähemmän kuin ylioppilas Otto. Hyvä yritys!', style: GoogleFonts.getFont('Lexend', color: const Color.fromARGB(255, 255, 255, 255)), textAlign: TextAlign.center,),
+        ],),
       ],
     );
   }
--- lib/widget_store.dart
+++ lib/widget_store.dart
@@ -4,162 +4,308 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'interactive.dart';
-// Yläosio 
+
+
+// ANCHOR Yläosio
 final tervetuloToivotus = Text(
   'Teidät on toivotettu tervetulleiksi',
-  style: GoogleFonts.getFont('Martel',fontSize: 24, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 255, 255, 255)),
+  style: GoogleFonts.getFont(
+    'Martel',
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
-final lakkiKuva = Image.asset('assets/images/YOlakkisuora.webp', width: 20, height: 20);
+final lakkiKuva = Image.asset(
+  'assets/images/YOlakkisuora.webp',
+  width: 20,
+  height: 20,
+);
 
-final lakkiKuvaIso = Image.asset('assets/images/YOlakkiisometric.png', width: 20, height: 20);
+final lakkiKuvaIso = Image.asset(
+  'assets/images/YOlakkiisometric.png',
+  width: 20,
+  height: 20,
+);
 
-final komitea = FittedBox(fit: BoxFit.scaleDown, child: Row(children: [lakkiKuva,tervetuloToivotus,lakkiKuvaIso], mainAxisAlignment: MainAxisAlignment.center));
-              
+final komitea = FittedBox(
+  fit: BoxFit.scaleDown,
+  child: Row(
+    children: [lakkiKuva, tervetuloToivotus, lakkiKuvaIso],
+    mainAxisAlignment: MainAxisAlignment.center,
+  ),
+);
+
 final kaunisTeksti = Text(
   'Oton ylioppilasjuhlaan',
-  style: GoogleFonts.getFont('Mea Culpa',fontSize: 45, color: const Color.fromARGB(255, 218, 204, 7)),
+  style: GoogleFonts.getFont(
+    'Mea Culpa',
+    fontSize: 45,
+    color: const Color.fromARGB(255, 218, 204, 7),
+  ),
 );
 
-final sijaintiLogo = Icon(Icons.pin_drop_outlined, size: 48, color: const Color.fromARGB(255, 89, 58, 204));
+final sijaintiLogo = Icon(
+  Icons.pin_drop_outlined,
+  size: 48,
+  color: const Color.fromARGB(255, 89, 58, 204),
+);
 
-final aikaLogo = Icon(Icons.calendar_month, size: 48, color: const Color.fromARGB(255, 89, 58, 204));
+final aikaLogo = Icon(
+  Icons.calendar_month,
+  size: 48,
+  color: const Color.fromARGB(255, 89, 58, 204),
+);
 
 final sijaintiLinkki = GestureDetector(
-            onTap: () async {
-              final Uri url = Uri.parse('https://maps.app.goo.gl/Evzre87stuKj8u3f7');
-              if (!await launchUrl(url)) {
-                throw Exception('Could not launch $url');
-              }
-            },
-            child: Text(
-              'Äijälätalo, Kangasala',
-              style: TextStyle(
-                fontSize: 18,
-                color: const Color.fromARGB(255, 89, 58, 204),
-                decoration: TextDecoration.underline, // Optional: Add underline
-              ),
-            ),
+  onTap: () async {
+    final Uri url = Uri.parse('https://maps.app.goo.gl/Evzre87stuKj8u3f7');
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  },
+  child: Text(
+    'Äijälätalo, Kangasala',
+    style: const TextStyle(
+      fontSize: 18,
+      color: const Color.fromARGB(255, 89, 58, 204),
+      decoration: TextDecoration.underline, // Optional: Add underline
+    ),
+  ),
 );
 
 final aikaTeksti = Text(
-'31.5. klo XX:00',
-style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 255, 255, 255)),
+  '31.5. klo XX:00',
+  style: const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
-//}
+
+// ANCHOR FAQ
 
 final faqOtsikko = Text(
   'FAQ',
-  style: GoogleFonts.getFont('Lexend',fontSize: 48, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 48,
+    fontWeight: FontWeight.bold,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final kysymys0 = Text(
   'Miten ilmoittaudun saapuvaksi?',
-  style: GoogleFonts.getFont('Lexend',fontSize: 21, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final vastaus0 = Text(
   'Ilmoittaudu viestillä kertoen, kuinka moni on tulossa paikalle teiltä.',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+  style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final kysymys1 = Text(
   'Onko tarjolla ruokaa?',
-  style: GoogleFonts.getFont('Lexend',fontSize: 21, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final vastaus1 = Text(
   'Tarjolla on kevyttä ruokaa ja herkullista herkkua.',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final kysymys2 = Text(
   'Erikoisruokavaliot?',
-  style: GoogleFonts.getFont('Lexend',fontSize: 21, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final vastaus2 = Text(
   'Lähes kaikille on jotakin, ilmoitathan harvinaislaatuisemmat rajoitteet etukäteen!',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final kysymys3 = Text(
   'Mitä toisin lahjaksi?',
-  style: GoogleFonts.getFont('Lexend',fontSize: 21, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final vastaus3 = Text(
   'Lahjaa ei tarvitse tuoda, jos haluat kuitenkin muistaa jollakin tässä ohjeet:',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 255, 255, 255)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
 );
 
 final vastaus31 = Text(
-  ' + Kylmä käteinen                               ',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 9, 255, 0)),
+  ' + Kylmä käteinen',
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 9, 255, 0),
+  ),
 );
 final vastaus32 = Text(
-  ' + Lahjakortit                                    ',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 9, 255, 0)),
+  ' + Lahjakortit',
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 9, 255, 0),
+  ),
 );
 final vastaus33 = Text(
   ' + Aineettomat lahjat (teatteriliput, tms)',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 9, 255, 0)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 9, 255, 0),
+  ),
 );
 final vastaus34 = Text(
   ' + Lahjoitukset (WWF, Punainen risti, animalia...)',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 9, 255, 0)),
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 9, 255, 0),
+  ),
 );
 final vastaus35 = Text(
-  ' - Aineelliset lahjat                             ',
-  style: GoogleFonts.getFont('Lexend',fontSize: 16, fontWeight: FontWeight.normal, color: const Color.fromARGB(255, 255, 0, 0)),
+  ' - Aineelliset lahjat',
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: const Color.fromARGB(255, 255, 0, 0),
+  ),
 );
 
 final column1Container = Container(
-  color: const Color.fromARGB(44, 122, 124, 125),
-  child: Center(child: SingleChildScrollView(
-    scrollDirection: Axis.vertical,
-    child: Column(children: [
-    faqOtsikko,
-    SizedBox(width: 10),
-    kysymys0,
-    vastaus0,
-    kysymys1,
-    vastaus1,
-    kysymys2,
-    vastaus2,
-    kysymys3,
-    vastaus3,
-    vastaus31,
-    vastaus32,
-    vastaus33,
-    vastaus34,
-    vastaus35 ]))));
+  color: const Color.fromARGB(44, 63, 106, 127),
+  child: Center(
+    child: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          faqOtsikko,
+          SizedBox(width: 10),
+          kysymys0,
+          vastaus0,
+          kysymys1,
+          vastaus1,
+          kysymys2,
+          vastaus2,
+          kysymys3,
+          vastaus3,
+          vastaus31,
+          vastaus32,
+          vastaus33,
+          vastaus34,
+          vastaus35,
+        ],
+      ),
+    ),
+  ),
+);
+
+// ANCHOR Peli
+
+final PeliOtsikko = Text(
+  'Sinustako ylioppilas?',
+  textAlign: TextAlign.center,
+    style: GoogleFonts.getFont(
+    'Lexend',
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    color: const Color.fromARGB(255, 255, 255, 255),
+  ),
+);
 
 final column2Container = Container(
-  color: Colors.orange,
-  child: Center(child: MyInteractiveText()),
-);
+  color: const Color.fromARGB(44, 121, 106, 75),
+  child: Center(
+  child: SingleChildScrollView(
+    scrollDirection: Axis.vertical,
+    child: Center(child: Column(children: [PeliOtsikko, SYOpeli()]))
+)));
 
 final columnSpacing = SizedBox(width: 20);
 
 final columnPadding = const EdgeInsets.all(8.0);
 
-
-final tikki = Column(children: [
-  SizedBox(height: 20),
-  Divider(
-    color: const Color.fromARGB(255, 255, 255, 255),
-    thickness: 2,
-    indent: 20,
-    endIndent: 20,
-    height: 1,
-  ), 
-  Divider(
-    color: const Color.fromARGB(255, 162, 162, 162),
-    thickness: 2,
-    indent: 20,
-    endIndent: 20,
-    height: 1,
-  ),
-  SizedBox(height: 20),]
+final tikki = Column(
+  children: [
+    SizedBox(height: 20),
+    Divider(
+      color: const Color.fromARGB(255, 255, 255, 255),
+      thickness: 2,
+      indent: 20,
+      endIndent: 20,
+      height: 1,
+    ),
+    Divider(
+      color: const Color.fromARGB(255, 162, 162, 162),
+      thickness: 2,
+      indent: 20,
+      endIndent: 20,
+      height: 1,
+    ),
+    SizedBox(height: 20),
+  ],
 );
